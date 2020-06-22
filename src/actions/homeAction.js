4c567a77ac87ed11aa443ad760aae5728854de74
pengzhenjin <562260994@qq.com>
2020-06-22 22:59:11 +0800

    update Home.js

--- src/actions/homeAction.js
+++ src/actions/homeAction.js
@@ -1,43 +1,38 @@
 import Api from '../api/Api';
 import actionType from './types/actionType';
 
-export const fetchHomeBanner = () => {
-
-  return dispatch => {
+export const fetchHomeBanner = () => dispatch => {
     console.log('dispatch：', dispatch);
     Api.getHomeBanner().then(response => {
-      dispatch({
-        type: actionType.FETCH_HOME_BANNER,
-        homeBanner: response.data,
-      });
+        dispatch({
+            type: actionType.FETCH_HOME_BANNER,
+            homeBanner: response.data,
+        });
     });
-  };
 };
 
-export const fetchHomeArticleList = () => {
-  Api.getHomeArticleList().then(response => {
-    // dispatch({
-    //   type: actionType.FETCH_HOME_ARTICLE_LIST,
-    //   homeArticleList: response.data,
-    // });
-  }).catch(error => {
-    // dispatch({
-    //   type: actionType.FETCH_HOME_ARTICLE_LIST_FAILURE,
-    // });
-  });
-};
-
-export const fetchHomeArticleListMore = (page) => {
-  return (dispatch) => {
-    Api.getHomeArticleList(page).then(response => {
-      dispatch({
-        type: actionType.FETCH_HOME_ARTICLE_LIST_MORE,
-        homeArticleList: response.data,
-      });
+export const fetchHomeArticleList = () => dispatch => {
+    Api.getHomeArticleList().then(response => {
+        dispatch({
+            type: actionType.FETCH_HOME_ARTICLE_LIST,
+            homeArticleList: response.data,
+        });
     }).catch(error => {
-      dispatch({
-        type: actionType.FETCH_HOME_ARTICLE_LIST_FAILURE,
-      });
+        dispatch({
+            type: actionType.FETCH_HOME_ARTICLE_LIST_FAILURE,
+        });
     });
-  };
+};
+
+export const fetchHomeArticleListMore = (page) => dispatch => {
+    Api.getHomeArticleList(page).then(response => {
+        dispatch({
+            type: actionType.FETCH_HOME_ARTICLE_LIST_MORE,
+            homeArticleList: response.data,
+        });
+    }).catch(error => {
+        dispatch({
+            type: actionType.FETCH_HOME_ARTICLE_LIST_FAILURE,
+        });
+    });
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,129 +2,134 @@
  * 首页
  */
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import BaseContainer from '../../base/BaseContainer';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import CommonFlatList from '../../components/CommonFlatList';
 import Banner from '../../components/Banner';
 import ListFooter from '../../components/ListFooter';
 import ArticleItemRow from '../../components/ArticleItemRow';
-import { fetchHomeBanner, fetchHomeArticleList, fetchHomeArticleListMore } from '../../actions/homeAction';
-import { bindActionCreators } from 'redux';
+import {fetchHomeBanner, fetchHomeArticleList, fetchHomeArticleListMore} from '../../actions/homeAction';
 
 class Home extends BaseContainer {
-  constructor (props) {
-    super(props);
-    this.state = {
-      isRefreshing: false,
-    };
-  }
-
-  componentDidMount () {
-    this.fetchData();
-  }
-
-  fetchData () {
-    fetchHomeBanner();
-    // fetchHomeArticleList();
-  }
-
-  renderItem = ({ item, index }) => {
-    return (
-      <ArticleItemRow
-        navigation={this.navigation}
-        item={item}
-        onCollectPress={() => {
-          // if (!isLogin) {
-          //   showToast(i18n('please-login-first'));
-          //   return navigation.navigate('Login');
-          // }
-          // if (item.collect) {
-          //   fetchHomeCancelCollect(item.id, index);
-          // } else {
-          //   fetchHomeAddCollect(item.id, index);
-          // }
-        }}
-      />
-    );
-  };
-
-  renderHeader = () => {
-    const { homeBanner } = this.props;
-    console.log('homeBanner：', homeBanner);
-    return (
-      <View>
-        <Banner bannerArr={homeBanner} navigation={this.navigation}/>
-        <View style={{ height: 20 }}/>
-      </View>
-    );
-  };
-
-  renderFooter = () => {
-    const { isRenderFooter, isFullData, themeColor } = this.props;
-    return (
-      <ListFooter
-        isRenderFooter={isRenderFooter}
-        isFullData={isFullData}
-        indicatorColor={themeColor}
-      />
-    );
-  };
-
-  onEndReached () {
-    const { isFullData, page } = this.props;
-    if (isFullData) {
-      return;
+    constructor(props) {
+        super(props);
+        this.state = {
+            isRefreshing: false,
+        };
     }
-    fetchHomeArticleListMore(page);
-  }
 
-  onRefresh () {
-    this.setState({ isRefreshing: true });
-    this.fetchData();
-    this.setState({ isRefreshing: false });
-  }
+    componentDidMount() {
+        this.fetchData();
+    }
 
-  render () {
-    return (
-      <View style={styles.container}>
-        <CommonFlatList
-          data={this.props.dataSource}
-          keyExtractor={item => item.id.toString()}
-          renderItem={this.renderItem}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
-          onEndReached={this.onEndReached.bind(this)}
-          toRefresh={this.onRefresh.bind(this)}
-          isRefreshing={this.state.isRefreshing}
-        />
-      </View>
-    );
-  }
+    fetchData() {
+      this.props.fetchHomeBanner();
+        // fetchHomeBanner();
+        // fetchHomeArticleList();
+    }
+
+    renderItem = ({item, index}) => {
+        return (
+            <ArticleItemRow
+                navigation={this.navigation}
+                item={item}
+                onCollectPress={() => {
+                    // if (!isLogin) {
+                    //   showToast(i18n('please-login-first'));
+                    //   return navigation.navigate('Login');
+                    // }
+                    // if (item.collect) {
+                    //   fetchHomeCancelCollect(item.id, index);
+                    // } else {
+                    //   fetchHomeAddCollect(item.id, index);
+                    // }
+                }}
+            />
+        );
+    };
+
+    renderHeader = () => {
+        const {homeBanner} = this.props;
+        console.log('homeBanner：', homeBanner);
+        return (
+            <View>
+                <Banner bannerArr={homeBanner} navigation={this.navigation}/>
+                <View style={{height: 20}}/>
+            </View>
+        );
+    };
+
+    renderFooter = () => {
+        const {isRenderFooter, isFullData, themeColor} = this.props;
+        return (
+            <ListFooter
+                isRenderFooter={isRenderFooter}
+                isFullData={isFullData}
+                indicatorColor={themeColor}
+            />
+        );
+    };
+
+    onEndReached() {
+        const {isFullData, page} = this.props;
+        if (isFullData) {
+            return;
+        }
+        fetchHomeArticleListMore(page);
+    }
+
+    onRefresh() {
+        this.setState({isRefreshing: true});
+        this.fetchData();
+        this.setState({isRefreshing: false});
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <CommonFlatList
+                    data={this.props.dataSource}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={this.renderItem}
+                    ListHeaderComponent={this.renderHeader}
+                    ListFooterComponent={this.renderFooter}
+                    onEndReached={this.onEndReached.bind(this)}
+                    toRefresh={this.onRefresh.bind(this)}
+                    isRefreshing={this.state.isRefreshing}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    page: state.homeReducer.page,
-    dataSource: state.homeReducer.dataSource,
-    homeBanner: state.homeReducer.homeBanner,
-    homeArticleList: state.homeReducer.homeArticleList,
-    isRenderFooter: state.homeReducer.isRenderFooter,
-    isFullData: state.homeReducer.isFullData,
-  };
+    return {
+        page: state.homeReducer.page,
+        dataSource: state.homeReducer.dataSource,
+        homeBanner: state.homeReducer.homeBanner,
+        homeArticleList: state.homeReducer.homeArticleList,
+        isRenderFooter: state.homeReducer.isRenderFooter,
+        isFullData: state.homeReducer.isFullData,
+    };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    fetchHomeBanner: bindActionCreators(fetchHomeBanner, dispatch),
-    fetchHomeArticleList: bindActionCreators(fetchHomeArticleList, dispatch),
-  };
+    // return {
+    //     fetchHomeBanner: bindActionCreators(fetchHomeBanner, dispatch),
+    //     fetchHomeArticleList: bindActionCreators(fetchHomeArticleList, dispatch),
+    // };
+    return {
+        fetchHomeBanner: () => dispatch(fetchHomeBanner()),
+        fetchHomeArticleList: () => dispatch(fetchHomeArticleList()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
